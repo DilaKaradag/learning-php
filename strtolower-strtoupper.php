cb0a5fa3dd27494e654b6283a958fbac18743372
Dila <165695398+DilaKaradag@users.noreply.github.com>
2024-12-15 16:22:54 +0300

    Create strtolower-strtoupper.php

---
+++ strtolower-strtoupper.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>abcd</title>
+</head>
+<body>
+    
+    
+    <?php
+    
+    $metin="Php de neler YapILABILIR?";
+    echo $metin."<br>";
+
+    $kucuk=strtolower($metin); //bütün metni küçültür.
+    echo $kucuk."<br>";
+
+    $buyuk=strtoupper($metin); //bütün metni büyültür
+    echo $buyuk;
+    ?>
+
+</body>
+</html>
